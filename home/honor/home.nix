bcb34a880c3f3a20249663158bf890ef135ca783
Don-Yin <don_yin@outlook.com>
2026-01-01 05:22:19 +0000

    update readme

--- home/honor/home.nix
+++ home/honor/home.nix
@@ -22,5 +22,10 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  programs.bash = {
+    enable = true;
+    profileExtra = builtins.readFile ./dotfiles/bash_profile.sh;
+  };
 }
 
