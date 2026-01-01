f0de547aa0a3e348a25266b180425c97636c25a8
Don-Yin <don_yin@outlook.com>
2026-01-01 06:09:31 +0000

    amend apps

--- modules/desktop/apps.nix
+++ modules/desktop/apps.nix
@@ -6,6 +6,8 @@
   programs.nix-ld.enable = true; # for nix-ld to work with cursor remote
   programs.hyprland = { enable = true; xwayland.enable = true; };
   services.power-profiles-daemon.enable = true;
+  services.gnome.gnome-keyring.enable = true;  # key management for gnome
+  security.pam.services.login.enableGnomeKeyring = true;  # enable keyring for login
 
   environment.systemPackages = with pkgs; [
     git
@@ -23,6 +25,7 @@
     glib # for gsettings
     pavucontrol
     helvum
+    seahorse # gui for gnome-keyring
   ];
 
   environment.sessionVariables = {
